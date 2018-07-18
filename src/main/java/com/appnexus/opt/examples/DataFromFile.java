22963629b9c0055ea3bcf906a565923be11e7b22
Noah Stebbins <nstebbins@appnexus.com>
2018-07-18 13:31:24 -0400

    VVAL-219: comments for DataFromFile

--- src/main/java/com/appnexus/opt/examples/DataFromFile.java
+++ src/main/java/com/appnexus/opt/examples/DataFromFile.java
@@ -2,13 +2,16 @@ package com.appnexus.opt.examples;
 
 import com.appnexus.opt.ml.SparseObservation;
 
-// TODO: documentation?
-class DataFromFile {
+/*
+ * This POJO contains all relevant data retrieved from a file that is
+ * necessary for training and testing the logistic regression algorithm.
+ */
+public class DataFromFile {
 
     private SparseObservation[] sparseObservations;
     private int numOfFeatures;
 
-    DataFromFile(SparseObservation[] sparseObservations, int numOfFeatures) {
+    public DataFromFile(SparseObservation[] sparseObservations, int numOfFeatures) {
         this.sparseObservations = sparseObservations;
         this.numOfFeatures = numOfFeatures;
     }
